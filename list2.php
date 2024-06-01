ae48922c8043f53b3c2d3080713a91b20d5e043c
KuRa04 <mwamkuracchi@gmail.com>
2024-06-01 16:11:32 +0900

    refactor: htmlのコードレイアウト修正

--- list2.php
+++ list2.php
@@ -1,112 +1,106 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8"/>
-<title>SwitchBotプロクシ（暗号化）</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<style>
-table {
-    border-collapse: collapse;
-}
-th, td {
-    padding: 5px 10px 5px 10px;
-    border-width: 1px 0px;
-    border-style: solid;
-    border-collapse: collapse;
-}
-textarea#encdata {
-    //display: none;
-} 
+    <head>
+        <meta charset="utf-8"/>
+        <title>SwitchBotプロクシ（暗号化）</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <style>
+            table {
+                border-collapse: collapse;
+            }
+            th, td {
+                padding: 5px 10px 5px 10px;
+                border-width: 1px 0px;
+                border-style: solid;
+                border-collapse: collapse;
+            }
+            textarea#encdata {
+                display: none;
+            } 
+        </style>
+    </head>
+    <body>
+        <?php
+            $pythonScriptPath = __DIR__ . '/index.py';
+            $token = $_REQUEST['token'];
+            $secret = $_REQUEST['secret'];
+            $output = [];
+            $return_variable = 0;
+            exec("python3 $pythonScriptPath $token $secret", $output, $return_variable);
+            // JSONレスポンスを結合してデコード
+            $deviceList = json_decode(implode("\n", $output), true);
+        ?>
 
-</style>
-</head>
-<body>
-<?php
-    $pythonScriptPath = __DIR__ . '/index.py';
-    $token = $_REQUEST['token'];
-    $secret = $_REQUEST['secret'];
-    $output = [];
-    $return_variable = 0;
-    exec("python3 $pythonScriptPath $token $secret", $output, $return_variable);
-    // JSONレスポンスを結合してデコード
-    $deviceList = json_decode(implode("\n", $output), true);
-?>
+        <form name="device">
+            <p>Device List:</p><br>
+            <table name="device">
+                <tr>
+                    <th><input type="checkbox" name="deviceall" value=""></th>
+                    <th>Device ID: </th>
+                    <th>Device Name: </th>
+                    <th>Device Type: </th>
+                    <th>Enable Cloud Service: </th>
+                    <th>Hub Device ID: </th>
+                </tr>
+                <?php
+                    foreach ((array)$deviceList['body']['deviceList'] as $device) {
+                        echo "<tr>\n";
+                        echo '<td><input type="checkbox" name="pick" value="' . $device['deviceId'] . '"></td>';
+                        echo "<td>" . $device['deviceId'] . "</td>\n";
+                        echo "<td>" . $device['deviceName'] . "</td>\n";
+                        echo "<td>" . $device['deviceType'] . "</td>\n";
+                        echo "<td>" . ($device['enableCloudService'] ? 'Yes' : 'No') . "</td>\n";
+                        echo "<td>" . $device['hubDeviceId'] . "</td>\n";
+                        echo "</tr>\n";
+                        echo "\n";
+                    }
+                ?>
+            </table>
+        </form>
+    <br/>
 
-<form name="device">
+    <!-- // infraredRemoteListの表示 -->
+        <form name="remote">
 
-Device List:<br>
-<table name="device">
-    <tr>
-        <th><input type="checkbox" name="deviceall" value=""></th>
-        <th>Device ID: </th>
-        <th>Device Name: </th>
-        <th>Device Type: </th>
-        <th>Enable Cloud Service: </th>
-        <th>Hub Device ID: </th>
-    </tr>
+            <p>Infrared Remote List:</p><br/>
+            <table name="remote">
+                <tr>
+                    <th><input type="checkbox" name="remoteall" value=""></th>
+                    <th>Device ID: </th>
+                    <th>Device Name: </th>
+                    <th>Remote Type: </th>
+                    <th>Hub Device ID: </th>
+                </tr>
+            </table>
+        </form>
+        <br/>
+        <hr/>
+        
+        <input type="button" value="　選択決定　" onclick="clickBtn()" />
+       
+        <table>
+            <tr><td>選択されたDeviceID</td><td><b><span id="dlist"></span></b></td></tr>
+            <tr><td>選択されたInfrared DeviceID</td><td><b><span id="rlist"></span></b></td></tr>
+        </table>
+        <hr/>
 
-    <?php
-        foreach ((array)$deviceList['body']['deviceList'] as $device) {
-            echo "<tr>\n";
-            echo '<td><input type="checkbox" name="pick" value="' . $device['deviceId'] . '"></td>';
-            echo "<td>" . $device['deviceId'] . "</td>\n";
-            echo "<td>" . $device['deviceName'] . "</td>\n";
-            echo "<td>" . $device['deviceType'] . "</td>\n";
-            echo "<td>" . ($device['enableCloudService'] ? 'Yes' : 'No') . "</td>\n";
-            echo "<td>" . $device['hubDeviceId'] . "</td>\n";
-            echo "</tr>\n";
-            //echo "\n";
-        }
-    ?>
-</table>
-</form>
-<br/>
+        <p>token:</p><br/>
+        <input type="text" name="token" id="token" size="100" value="<?php echo $token ?>"/><br/>
 
-<!-- // infraredRemoteListの表示 -->
-<form name="remote">
+        <p>SwitchBotプロクシの利用パスワード：</p><br/>
+        <input type="text" name="password" id="password"/><br/>
+        <input type="button" value="　暗号化　" onclick="clickBtnEnc()" />
 
-Infrared Remote List:<br/>
-<table name="remote">
-<tr>
-<th><input type="checkbox" name="remoteall" value=""></th>
-<th>Device ID: </th>
-<th>Device Name: </th>
-<th>Remote Type: </th>
-<th>Hub Device ID: </th>
-</tr>
+        <br/><br/>
+        <script type="text/javascript" src="sw.js"></script>
 
-</table>
-</form>
-<br/>
-<hr/>
-
-<input type="button" value="　選択決定　" onclick="clickBtn()" />
-
-<table>
-<tr><td>選択されたDeviceID</td><td><b><span id="dlist"></span></b></td></tr>
-<tr><td>選択されたInfrared DeviceID</td><td><b><span id="rlist"></span></b></td></tr>
-</table>
-
-<hr/>
-
-token: <br/>
-<input type="text" name="token" id="token" size="100" value="<?php echo $token ?>"/><br/>
-
-SwitchBotプロクシの利用パスワード：<br/>
-<input type="text" name="password" id="password"/><br/>
-<input type="button" value="　暗号化　" onclick="clickBtnEnc()" />
-
-<br/><br/>
-<script type="text/javascript" src="sw.js"></script>
-
-暗号化データ<br/>
-<textarea id="encdata" cols="100" rows="5" readonly></textarea>
-<hr/>
-復号化の確認<br/>
-<input type="button" value="　復号化して確認　" onclick="clickBtnDec()" /><br/>
-<textarea id="decdata" cols="100" rows="10" readonly></textarea>
-<hr/>
-<p><small>&copy; 2023 watalab.info</small></p>
-
-</body>
+        <p>暗号化データ</p><br/>
+        <textarea id="encdata" cols="100" rows="5" readonly></textarea>
+        <hr/>
+        <p>復号化の確認</p><br/>
+        <input type="button" value="　復号化して確認　" onclick="clickBtnDec()" /><br/>
+        <textarea id="decdata" cols="100" rows="10" readonly></textarea>
+        <hr/>
+        <p><small>&copy; 2023 watalab.info</small></p>
+    </body>
 </html>
