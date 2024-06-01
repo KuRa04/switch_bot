bf1096d666f0d8a3c6ca83518a5d6b042524036d
KuRa04 <mwamkuracchi@gmail.com>
2024-06-01 23:36:31 +0900

    feat: 認証をutilに修正

--- index.py
+++ index.py
@@ -1,51 +1,28 @@
 import time
-import hashlib
-import hmac
-import base64
-import uuid
 import requests
-import pprint
 import json
 import sys
 import logging
-
-def make_secret(secret_key):
-    secret_key = bytes(secret_key, 'utf-8')
-    return secret_key
-
-def make_sign(secret_key, t, nonce):
-    string_to_sign = '{}{}{}'.format(token, t, nonce)
-    string_to_sign = bytes(string_to_sign, 'utf-8')
-    sign = base64.b64encode(hmac.new(secret_key, msg=string_to_sign, digestmod=hashlib.sha256).digest())
-    return sign
-
-def make_t():
-    t = int(round(time.time() * 1000))
-    return str(t)
-
-def make_nonce():
-    nonce = str(uuid.uuid4())
-    return nonce
+from utils import make_secret, make_sign, make_t, make_nonce
 
 if len(sys.argv) != 3:
     print(json.dumps({"error": "引数が不正です"}))
     sys.exit(1)
 
-
-#SwitchBotアプリから取得
+# SwitchBotアプリから取得
 token = sys.argv[1]
 secret_key = sys.argv[2]
 
-#Requestパラメータ作成
+# Requestパラメータ作成
 secret_key = make_secret(secret_key)
 t = make_t()
 nonce = make_nonce()
-sign = make_sign(secret_key, t, nonce)
+sign = make_sign(secret_key, token, t, nonce)  # token を引数として渡す
 
-#URL指定
+# URL指定
 url = "https://api.switch-bot.com/v1.1/devices"
 
-#APIheader作成
+# APIheader作成
 headers = {
     "Authorization": token,
     "sign": sign,
@@ -54,7 +31,7 @@ headers = {
     "Content-Type": "application/json; charset=utf-8"
 }
 
-#requests処理
-response = requests.get(url,headers=headers)
+# requests処理
+response = requests.get(url, headers=headers)
 
 print(json.dumps(response.json()))
