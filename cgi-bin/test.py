1ca4541ccc753d4ba05354ef777e4c1fd392a011
KuRa04 <mwamkuracchi@gmail.com>
2024-06-03 22:31:21 +0900

    feat: テスト用のcgiファイルを追加

---
+++ cgi-bin/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cgi
+
+print("Content-Type: text/html; charset=utf-8")
+print()
+print("<html><head>")
+print("<title>CGI Script</title>")
+print("</head><body>")
+print("<h1>こんにちは、CGIの世界！</h1>")
+print("</body></html>")
