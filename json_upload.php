ad75298b6a5cab0271a3a078abbfda6475cbe333
KuRa04 <mwamkuracchi@gmail.com>
2024-06-13 16:40:03 +0900

    feat: 復号化したJSONを表示

--- json_upload.php
+++ json_upload.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'encrypt_decrypt.php';    // encrypt, decrypt 関数が定義されているファイル
+
 session_start();
 
 // セッションからmanage_passwordを取得
@@ -19,7 +21,16 @@ if (isset($_POST['password']) && isset($_FILES['jsonFile'])) {
   if ($jsonFile['error'] == UPLOAD_ERR_OK) {
     $jsonContent = file_get_contents($jsonFile['tmp_name']);
     $data = json_decode($jsonContent, true);
-    echo $data['token']; //権限付与トークンを表示
+    $auth_guest_token = $data['token']; //権限付与トークン
+    $bin_password = hex2bin($manage_password);
+    $decrypt_password = $password . $bin_password; //復号化に使用するパスワード
+
+    //復号化処理
+    $dec = decrypt($auth_guest_token, $decrypt_password);
+    $dec_json = json_decode($dec, true);
+    $token = $dec_json['token']; //元々のswitchbotAPIのトークン
+    print_r($dec_json); //復号化したJSONデータを表示
+
 
     // ここで$manage_passwordと$form_passwordを使用した処理を行う
   } else {
