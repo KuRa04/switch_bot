fec0698c585d0c3fd8c364d0ed55ba8fba6298a0
KuRa04 <mwamkuracchi@gmail.com>
2024-06-21 15:37:30 +0900

    feat: status, commandをデバイスに紐づける

--- api/get_device_list.php
+++ api/get_device_list.php
@@ -30,8 +30,53 @@ function get_device_list()
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   $response = curl_exec($ch);
   curl_close($ch);
-
   return $response;
 }
+
+function readCsvToArray($filename)
+{
+  $csv = array_map('str_getcsv', file($filename));
+  array_walk($csv, function (&$a) use ($csv) {
+    $a = array_combine($csv[0], $a);
+  });
+  array_shift($csv); // remove column header
+  return $csv;
+}
+
+function addDataToDeviceList(&$deviceList, $csv, $dataType)
+{
+  foreach ($deviceList['body']['deviceList'] as $index => $device) {
+    foreach ($csv as $row) {
+      if ($device['deviceType'] == $row['deviceType']) {
+        if ($dataType == 'command') {
+          $deviceList['body']['deviceList'][$index]['commands'][] = [
+            'command' => $row['command'],
+            'commandParameter' => $row['commandParameter'],
+            'description' => $row['description']
+          ];
+        } elseif ($dataType == 'status' && stripos($row['key'], 'device') === false) {
+          $deviceList['body']['deviceList'][$index]['status'][] = [
+            'key' => $row['key'],
+            'deviceType' => $row['deviceType'],
+            'description' => $row['description']
+          ];
+        }
+      }
+    }
+  }
+}
+
+// CSVファイルからデータを読み込む
+$commandCsv = readCsvToArray('../csv/command_type_table.csv');
+$statusCsv = readCsvToArray('../csv/status_type_table.csv');
+
+// $device_list_add_commandsを$device_listのコピーとして初期化
+$device_list = json_decode(get_device_list(), true);
+
+// コマンドデータを追加
+addDataToDeviceList($device_list, $commandCsv, 'command');
+// ステータスデータを追加
+addDataToDeviceList($device_list, $statusCsv, 'status');
+
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode(get_device_list());
+echo json_encode($device_list);
