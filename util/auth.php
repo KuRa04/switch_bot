5b5e298dd758c5f076c5bafe38db272869eb71b2
KuRa04 <mwamkuracchi@gmail.com>
2024-06-21 10:42:13 +0900

    feat: switchBotAPIのauthメソッドを分割

---
+++ util/auth.php
@@ -0,0 +1,17 @@
+<!-- switchBotAPIに必要な情報を作成 -->
+
+<?php
+function make_sign($secret_key, $token, $t, $nonce)
+{
+  return base64_encode(hash_hmac('sha256', $token . $t . $nonce, $secret_key, true));
+}
+
+function make_t()
+{
+  return round(microtime(true) * 1000);
+}
+
+function make_nonce()
+{
+  return bin2hex(random_bytes(16));
+}
