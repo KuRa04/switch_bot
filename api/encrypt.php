058bb046c1f3f193f7af3f5d9f78e829f2a80f97
KuRa04 <mwamkuracchi@gmail.com>
2024-07-04 16:31:50 +0900

    feat: 暗号化/復号化を関数化

--- api/encrypt.php
+++ api/encrypt.php
@@ -4,47 +4,53 @@ require_once __DIR__ . '/../constants/constants.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header("Content-Type: text/plain");
   exit();
 }
 
-$data = json_decode(file_get_contents('php://input'), true);
-$response = array();
+function encrypt()
+{
+  $data = json_decode(file_get_contents('php://input'), true);
+  $response = array();
 
-$token = $data['token'];
-$secret_key = $data['secretKey'];
-$password = $data['password'];
-$desc = $data['description'];
-$start_time = $data['startTime'];
-$end_time = $data['endTime'];
-$version = $data['version'];
-$vendor = $data['vendor'];
-$device_list = $data['deviceList'];
+  $token = $data['token'];
+  $secret_key = $data['secretKey'];
+  $password = $data['password'];
+  $desc = $data['description'];
+  $start_time = $data['startTime'];
+  $end_time = $data['endTime'];
+  $version = $data['version'];
+  $vendor = $data['vendor'];
+  $device_list = $data['deviceList'];
 
-if (!$token || !$password || !$device_list) {
-  $response = array("error" => "Parameters are not enough");
-} else {
-  $json_data = json_encode(array(
-    "token" => $token,
-    "secretKey" => $secret_key,
-    "description" => $desc,
-    "startTime" => $start_time,
-    "endTime" => $end_time,
-    "version" => $version,
-    "vendor" => $vendor,
-    "deviceList" => $device_list
-  ));
+  if (!$token || !$password || !$device_list) {
+    $response = array("error" => "Parameters are not enough");
+  } else {
+    $json_data = json_encode(array(
+      "token" => $token,
+      "secretKey" => $secret_key,
+      "description" => $desc,
+      "startTime" => $start_time,
+      "endTime" => $end_time,
+      "version" => $version,
+      "vendor" => $vendor,
+      "deviceList" => $device_list
+    ));
 
-  try {
-    $encrypt_password = $password . MANAGE_PASSWORD; // MANAGE_PASSWORDはconstants/constants.phpに定義
-    $encode_data = base64_encode(openssl_encrypt($json_data, 'aes-256-cbc', $encrypt_password, OPENSSL_RAW_DATA, 'iv12345678901234'));
-    $response = array("encodeData" => $encode_data);
-  } catch (Exception $e) {
-    $response = array("error" => "Encryption failed");
+    try {
+      $encrypt_password = $password . MANAGE_PASSWORD; // MANAGE_PASSWORDはconstants/constants.phpに定義
+      $encode_data = base64_encode(openssl_encrypt($json_data, 'aes-256-cbc', $encrypt_password, OPENSSL_RAW_DATA, 'iv12345678901234'));
+      $response = array("encodeData" => $encode_data);
+    } catch (Exception $e) {
+      $response = array("error" => "Encryption failed");
+    }
   }
+
+  return $response;
 }
 
-header("Content-Type: application/json; charset=UTF-8");
-echo json_encode($response);
+echo json_encode(encrypt());
