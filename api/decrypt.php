12c37200f67baf3a698c30c6a97244d59fd6c000
KuRa04 <mwamkuracchi@gmail.com>
2024-07-04 11:26:08 +0900

    feat: 管理者パスワードを用いて復号化

--- api/decrypt.php
+++ api/decrypt.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/../constants/constants.php';
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
@@ -13,8 +15,9 @@ $response = array();
 
 $encode_data = $data['encodeData'];
 $password = $data['password'];
+$decrypt_password = $password . MANAGE_PASSWORD;
 
-$response = openssl_decrypt(base64_decode($encode_data), 'aes-256-cbc', $password, OPENSSL_RAW_DATA, 'iv12345678901234');
+$response = openssl_decrypt(base64_decode($encode_data), 'aes-256-cbc', $decrypt_password, OPENSSL_RAW_DATA, 'iv12345678901234');
 
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($response);
